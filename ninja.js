2352afb3c699db313cb3a46665b489a0b18ba1ae
aviolette <aviolette@gmail.com>
2013-12-18 22:08:39 -0600

    Added headers to PUT request

--- ninja.js
+++ ninja.js
@@ -38,7 +38,7 @@ function requestFoodTruckData(callback) {
  * Sets the color on the ninja block eyes.
  */
 function setColor(color) {
-  var req = https.request({hostname : 'api.ninja.is', method: 'PUT', path: '/rest/v0/device/'+ deviceId + '?user_access_token=' + ninjaKey}, function(res) {
+  var req = https.request({hostname : 'api.ninja.is', headers: {"content-type" : "application/json" }, method: 'PUT', path: '/rest/v0/device/'+ deviceId + '?user_access_token=' + ninjaKey}, function(res) {
   });
 
   var body = {
